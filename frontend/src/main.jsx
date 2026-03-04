cdf99d7d42021191d886f3ac843684fcf0c2413e
Pranav Pasula <pranavpasula@gmail.com>
2026-03-04 12:01:35 +0000

    Trying HashRouter instead of BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client'
 import "./index.css";
 import App from './components/App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import { AuthProvider } from './Auth.jsx';
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
 	<QueryClientProvider client={queryClient}>
-		<BrowserRouter>
+		<Router>
 			<AuthProvider>
 				<App />
 			</AuthProvider>
-		</BrowserRouter>
+		</Router>
 	</QueryClientProvider>
 )
